8d5dfdc2aa4631b8adc6420f82e05934dc5aaa43
JakeServin <jakeservin@gmail.com>
2022-08-30 17:41:53 -0600

    created bathroom model

---
+++ server/models/bathroom.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+
+const BathroomSchema = new mongoose.Schema({
+	name: {
+		type: String,
+		required: true,
+	},
+	type: {
+		type: String,
+		required: true,
+	},
+	lat: {
+		type: Number,
+		required: true,
+	},
+	long: {
+		type: Number,
+		required: true,
+	},
+	description: {
+		type: String,
+		required: true,
+	},
+});
+
+const BathroomModel = mongoose.model("bathroom", BathroomSchema);
+
+module.exports = BathroomModel;
