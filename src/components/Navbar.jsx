03ab288d7444ccfade9cba14fa257710e7defcc4
Jake Servin <100403428+JakeServin@users.noreply.github.com>
2022-09-13 22:05:22 -0700

    centered navbar on y axis

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,44 +3,50 @@ import picture7 from "../../src/pictures/icon.png"
 
 const Navbar = () => {
   return (
-    <nav class="navbar sticky-top navbar-expand-sm navbar-dark bg-primary">
-        <div class="container">
-            <a href="#" class="navbar-brand mb-0 h1"><img 
-                class="d-inline-block align-top"
-                src={picture7} width="30" height="30"/>
-                Gotta Go
-            </a>
-            <button 
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarNav"
-            class="navbar-toggler"
-            aria-controls="navbarNav"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-            >
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div 
-                class="collapse navbar-collapse" 
-                id="navbarNav">
-                <ul class="navbar-nav">
-                    <li class="nav-item active">
-                        <a href="/" class="nav-link active">Home</a>
-                    </li>
-                    <li class="nav-item active">
-                        <a href="/about" class="nav-link">About</a>
-                    </li>
-                    <li class="nav-item active">
-                        <a href="/find" class="nav-link">Find a Bathroom</a>
-                    </li>
-
-                </ul>
-        </div>
-        
-        </div>
-    </nav>
-  )
+		<nav className="navbar sticky-top navbar-expand-sm navbar-dark bg-primary">
+			<div className="container d-flex align-items-center">
+				<a href="/" className="navbar-brand mb-0 h1 d-flex align-items-center">
+					<img
+						className="d-inline-block align-top"
+						src={picture7}
+						width="30"
+						height="30"
+					/>
+					<span>GottaGo</span>
+				</a>
+				<button
+					type="button"
+					data-bs-toggle="collapse"
+					data-bs-target="#navbarNav"
+					className="navbar-toggler"
+					aria-controls="navbarNav"
+					aria-expanded="false"
+					aria-label="Toggle navigation"
+				>
+					<span className="navbar-toggler-icon"></span>
+				</button>
+				<div className="collapse navbar-collapse" id="navbarNav">
+					<ul className="navbar-nav">
+						<li className="nav-item active">
+							<a href="/" className="nav-link active">
+								Home
+							</a>
+						</li>
+						<li className="nav-item active">
+							<a href="/about" className="nav-link">
+								About
+							</a>
+						</li>
+						<li className="nav-item active">
+							<a href="/find" className="nav-link">
+								Find a Bathroom
+							</a>
+						</li>
+					</ul>
+				</div>
+			</div>
+		</nav>
+  );
 }
 
 export default Navbar
