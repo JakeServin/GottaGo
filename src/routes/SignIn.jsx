c5061bcf6095b1dff39079d172eea8c0d45df81f
Jake Servin <100403428+JakeServin@users.noreply.github.com>
2022-09-17 11:39:17 -0700

    trying without credentials

--- src/routes/SignIn.jsx
+++ src/routes/SignIn.jsx
@@ -19,7 +19,7 @@ const SignIn = (props) => {
 		const signedIn = await Axios({
 			method: "POST",
 			data: user,
-			withCredentials: true,
+			// withCredentials: true,
 			url: "https://gotta-go-app.herokuapp.com/login_user",
 		}).then((res) => res.data);
 		console.log(typeof signedIn);
