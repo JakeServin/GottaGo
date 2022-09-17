c125813d8b905b009d375d4bd1ae0091edb45a71
Jake Servin <100403428+JakeServin@users.noreply.github.com>
2022-09-17 10:41:22 -0700

    prevent unnecessary zooming

--- src/routes/Find.jsx
+++ src/routes/Find.jsx
@@ -28,6 +28,42 @@ let mapOptions = {
 	zoom: 15,
 };
 
+// Ensure touches occur rapidly
+const delay = 500
+// Sequential touches must be in close vicinity
+const minZoomTouchDelta = 10
+
+// Track state of the last touch
+let lastTapAt = 0
+let lastClientX = 0
+let lastClientY = 0
+
+const preventDoubleTapZoom = (event)=> {
+  // Exit early if this involves more than one finger (e.g. pinch to zoom)
+  if (event.touches.length > 1) {
+    return
+  }
+
+  const tapAt = new Date().getTime()
+  const timeDiff = tapAt - lastTapAt
+  const { clientX, clientY } = event.touches[0]
+  const xDiff = Math.abs(lastClientX - clientX)
+  const yDiff = Math.abs(lastClientY - clientY)
+  if (
+    xDiff < minZoomTouchDelta &&
+    yDiff < minZoomTouchDelta &&
+    event.touches.length === 1 &&
+    timeDiff < delay
+  ) {
+    event.preventDefault()
+    // Trigger a fake click for the tap we just prevented
+    event.target.click()
+  }
+  lastClientX = clientX
+  lastClientY = clientY
+  lastTapAt = tapAt
+}
+
 const Find = () => {
 	const isLoaded  = true
 
@@ -108,10 +144,8 @@ const Find = () => {
 						})}
 						{newMarker ? <NewMarker marker={newMarker} /> : null}
 					</>
-					<StandaloneSearchBox>
-						<div
-							className="find-wrapper col text-center d-flex justify-content-center align-items-center flex-grow"
-						>
+					<StandaloneSearchBox onTouchStart={preventDoubleTapZoom}>
+						<div className="find-wrapper col text-center d-flex justify-content-center align-items-center flex-grow">
 							<div
 								className="align-self-end"
 								style={{
